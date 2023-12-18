313008e9e3c77b07f15c1f507feed0bf28c7db3d
Ahmed Abdel Rahim <ahmed.abdelrahim11a@gmail.com>
2023-12-18 23:44:49 +0200

    Professors are separated from meetings

--- classes/professors.py
+++ classes/professors.py
@@ -1,6 +1,4 @@
 from users import *
-import requests
-import json
 
 class professors(users):
     def sign_in(self, username, password):
@@ -17,27 +15,3 @@ class professors(users):
             self.sign_in(username, password)
         elif choice == "Sign up":
             self.sign_up(username, email, password)
-
-    def create_teams_meeting(self, token, user_id):
-        url = f"https://graph.microsoft.com/v1.0/users/{user_id}/onlineMeetings"
-
-        headers = {
-            'Authorization': 'Bearer ' + token,
-            'Content-Type': 'application/json'
-        }
-
-        payload = json.dumps({
-            "startDateTime":"2022-07-12T14:30:34.2444915-07:00",
-            "endDateTime":"2022-07-12T15:00:34.2464912-07:00",
-            "subject":"User Meeting"
-        })
-
-        response = requests.request("POST", url, headers=headers, data=payload)
-
-        if response.status_code == 201:
-            meeting_url = response.json()['joinWebUrl']
-            print(f'Meeting created: {meeting_url}')
-            return meeting_url
-        else:
-            print(f'Could not create meeting, status code: {response.status_code}')
-            return None
