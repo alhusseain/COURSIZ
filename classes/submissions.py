7b4a6fd5d132203da66b925d31d8ec51db01a13e
Ahmed Abdel Rahim <ahmed.abdelrahim11a@gmail.com>
2023-12-19 09:40:13 +0200

    Logic of quizzes is initially implemented

--- classes/submissions.py
+++ classes/submissions.py
@@ -4,7 +4,7 @@ import random
 import time
 
 c = connection.cursor()
-class submissions(students, ):
+class submissions():
     def submitWork(self, workLink, student):
         submissionID = random.randint(1 , 10000)
         if workLink.startswith("https://docs.google.com/") or workLink.startswith("https://drive.google.com/") and workLink.endswith("sharing"):
@@ -20,14 +20,25 @@ class submissions(students, ):
         else:
             print("Invalid link")
             self.submitWork(input("Enter a valid link: "), student)
-            
-    # def submitQuiz(self, durationInMinutes, is_started, student):
-    #     if isStarted:
-    #         print("Quiz has started")
-    #         time.sleep(durationInMinutes * 60)
-    #     if isFinished:
-    #         c.execute('''insert into Enroll_in(Student_ID, Course_Code, grade)
-    #         values (?, ?, ?, ?)''', (random.randint(1,10000), EnrollIn.courseID,))
-    #     conn.commit()
-    #     conn.close()
-    #     print("Quiz submitted successfully")
+    
+    # when the student enter the link of the quiz, the setTimeForQuiz function will be called using get method linked by clicking on the link
+    def setTimeForQuiz(self, durationInMinutes):
+        # setting timer on
+        isStarted = True
+        isFinished = False
+        isEnded = False
+        # using google sheets api, when the student submit the quiz, the isFinished will be set to True
+        isFinished = True
+        if isFinished:
+            isStarted = False
+            print("Quiz has ended")
+        # when the durationInMinutes is over, the isEnded will be set to True
+        isEnded = True
+        if not isFinished and isEnded:
+            # close the forms using google forms API
+            print("Quiz has ended")
+        # add the grade to the database from the google sheets using google sheets API
+        c.execute('''insert into Enroll_in(Student_ID, Course_Code, grade)
+            values (?, ?, ?, ?)''', (random.randint(1,10000), EnrollIn.courseID,))
+        c.commit()
+        c.close()
