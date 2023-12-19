ace3806b35b3a02b6e88af2306d7df73afdb2ba3
Ahmed Abdel Rahim <ahmed.abdelrahim11a@gmail.com>
2023-12-19 05:56:51 +0200

    Commented sign in

--- GUI/sign-in/signin.py
+++ GUI/sign-in/signin.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-
+import datetime
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
 
@@ -19,13 +19,18 @@ def sign_in():
 
     return redirect(url_for('index'))
 
+# users must choose whether they are students or teachers
+# it must be able to insert the data into the database
+# it must be able to check if the data is valid (username must be unique, password must be strong using regex, email must be valid using regex)
+# it must store the account creation date and time
 @app.route('/signup', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
 
-        flash(f'Username: {username}, Password: {password} created successfully!', 'success')
+        flash(f'Username: {username}, Email: {email},Password: {password} created successfully!', 'success')
         return redirect(url_for('index'))
 
     return render_template('signup.html')
