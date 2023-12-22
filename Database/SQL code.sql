2e9b52ac810f6ec1e13393363f7f030a54549224
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2023-12-22 05:29:37 +0200

    Update SQL code.sql

--- Database/SQL code.sql
+++ Database/SQL code.sql
@@ -1,41 +1,43 @@
 CREATE TABLE Users
 (
-  Email VARCHAR NOT NULL,
-  User_Type VARCHAR NOT NULL,
-  Account_creation_date VARCHAR NOT NULL,
+  First_Name VARCHAR(MAX),
+  Last_Name  VARCHAR(MAX),
+  Email VARCHAR(MAX) NOT NULL,
+  User_Type VARCHAR(MAX) NOT NULL,
+  Account_creation_date VARCHAR(MAX) NOT NULL,
   PRIMARY KEY (Email)
 );
 
 CREATE TABLE Students
 (
   Student_ID INT NOT NULL,
-  First VARCHAR NOT NULL,
-  Last VARCHAR NOT NULL,
-  Year VARCHAR NOT NULL,
-  attendanceRecord INT NOT NULL,
-  GPA FLOAT NOT NULL,
-  performancePoints INT NOT NULL,
-  Email VARCHAR NOT NULL,
+  /*First VARCHAR NOT NULL,
+  Last VARCHAR NOT NULL,*/
+  Year VARCHAR(MAX) ,
+  attendanceRecord INT,
+  GPA FLOAT ,
+  performancePoints INT ,
+  Email VARCHAR(MAX) NOT NULL,
   PRIMARY KEY (Student_ID),
   FOREIGN KEY (Email) REFERENCES Users(Email)
 );
 
 CREATE TABLE Teachers
 (
-  FirstName VARCHAR NOT NULL,
-  LastName VARCHAR NOT NULL,
+  /*FirstName VARCHAR NOT NULL,
+  LastName VARCHAR NOT NULL,*/
   TeacherID INT NOT NULL,
-  Email VARCHAR NOT NULL,
+  Email VARCHAR(MAX) NOT NULL,
   PRIMARY KEY (TeacherID),
   FOREIGN KEY (Email) REFERENCES Users(Email)
 );
 
 CREATE TABLE SuperVisors
 (
-  FirstName VARCHAR NOT NULL,
-  LastName VARCHAR NOT NULL,
+  /*FirstName VARCHAR NOT NULL,
+  LastName VARCHAR NOT NULL,*/
   SupervisorID INT NOT NULL,
-  Email VARCHAR NOT NULL,
+  Email VARCHAR(MAX) NOT NULL,
   PRIMARY KEY (SupervisorID),
   FOREIGN KEY (Email) REFERENCES Users(Email)
 );
@@ -43,13 +45,13 @@ CREATE TABLE SuperVisors
 
 CREATE TABLE Courses
 (
-  Name VARCHAR NOT NULL,
+  Name VARCHAR(MAX) NOT NULL,
   Year INT NOT NULL,
-  Semester VARCHAR NOT NULL,
-  Course_Code VARCHAR NOT NULL,
-  Capacity INT NOT NULL,
-  Number_enrolled INT NOT NULL,
-  TeacherID INT NOT NULL,
+  Semester VARCHAR(MAX) ,
+  Course_Code VARCHAR(MAX) NOT NULL,
+  Capacity INT ,
+  Number_enrolled INT ,
+  TeacherID INT ,
   /*SupervisorID INT NOT NULL,*/
   /*Student_ID INT NOT NULL,*/
   PRIMARY KEY (Course_Code),
@@ -61,12 +63,12 @@ CREATE TABLE Courses
 CREATE TABLE Uploads
 (
   Upload_ID INT NOT NULL,
-  Course_code VARCHAR NOT NULL,
+  Course_code VARCHAR(MAX) NOT NULL,
   Date_of_Upload Date NOT NULL,
-  Upload_header VARCHAR NOT NULL,
-  Upload_type VARCHAR NOT NULL,
-  Upload_description VARCHAR NOT NULL,
-  Document_link VARCHAR NOT NULL,
+  Upload_header VARCHAR(MAX) NOT NULL,
+  Upload_type VARCHAR(MAX) NOT NULL,
+  Upload_description VARCHAR(MAX),
+  Document_link VARCHAR(MAX),
   PRIMARY KEY (Upload_ID),
   FOREIGN KEY (Course_Code) REFERENCES Courses(Course_Code)
 );
@@ -74,9 +76,9 @@ CREATE TABLE Uploads
 CREATE TABLE Sumbissions
 (
   Submission_ID INT NOT NULL UNIQUE,
-  Submission_type VARCHAR NOT NULL,
-  Deadline_Date Date NOT NULL,
-  Document_link VARCHAR NOT NULL,
+  Submission_type VARCHAR(MAX) NOT NULL,
+  Deadline_Date Date,
+  Document_link VARCHAR(MAX),
   /*Upload_ID INT NOT NULL,*/
   /*PRIMARY KEY (Submission_ID),*/
   FOREIGN KEY (Submission_ID) REFERENCES Uploads(Upload_ID)
@@ -85,10 +87,11 @@ CREATE TABLE Sumbissions
 CREATE TABLE Announcements
 (
   Submission_ID INT NOT NULL,
-  Announcement VARCHAR NOT NULL,
+  Announcement VARCHAR(MAX),
   TeacherID INT ,
   Student_ID INT ,
   SupervisorID INT ,
+  Document_link VARCHAR(MAX),
   FOREIGN KEY (Submission_ID) REFERENCES Sumbissions(Submission_ID),
   FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID),
   FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID),
@@ -98,8 +101,8 @@ CREATE TABLE Announcements
 CREATE TABLE Enroll_in
 (
   Student_ID INT NOT NULL,
-  Course_Code VARCHAR NOT NULL,
-  grade INT,
+  Course_Code VARCHAR(MAX) NOT NULL,
+  final_grade float,
   PRIMARY KEY (Student_ID, Course_Code),
   FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID),
   FOREIGN KEY (Course_Code) REFERENCES Courses(Course_Code)
