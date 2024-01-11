62f0d8714f5a52e283584117332f934a7d827884
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2024-01-11 05:27:17 +0200

    Update Upload.py

--- classes/Upload.py
+++ classes/Upload.py
@@ -1,8 +1,7 @@
 import pyodbc
-import pyodbc
-connection=pyodbc.connect('Driver={SQL Server};SERVER=KAREEM;DATABASE=Coursiz;Trusted_Connection=yes')
-query = connection.cursor()
+from databaseConnection import db
 from datetime import datetime
+db=db()
 
 class upload:
     def __init__(self):
@@ -14,28 +13,28 @@ class upload:
         self.up_date=[]
     def upload_info(self,course_code):
             self.course_code=course_code
-            query.execute("select Upload_ID from Uploads where Course_code = ?",self.course_code )
-            for row in query.fetchall():
+            db.cursor.execute("select Upload_ID from Uploads where Course_code = ?",self.course_code )
+            for row in db.cursor.fetchall():
                 self.up_id.append(row[0])
 
-            query.execute("select Upload_header from Uploads where Course_code = ?", (self.course_code))
-            for row in query.fetchall():
+            db.cursor.execute("select Upload_header from Uploads where Course_code = ?", (self.course_code))
+            for row in db.cursor.fetchall():
                 self.up_header.append(row[0])
             
-            query.execute("select Upload_type from Uploads where Course_code = ?", (self.course_code))
-            for row in query.fetchall():
+            db.cursor.execute("select Upload_type from Uploads where Course_code = ?", (self.course_code))
+            for row in db.cursor.fetchall():
                 self.up_type.append(row[0])
             
-            query.execute("select Upload_description from Uploads where Course_code = ?", (self.course_code))
-            for row in query.fetchall():
+            db.cursor.execute("select Upload_description from Uploads where Course_code = ?", (self.course_code))
+            for row in db.cursor.fetchall():
                 self.up_desc.append(row[0])
             
-            query.execute("select Document_link from Uploads where Course_code = ?", (self.course_code))
-            for row in query.fetchall():
+            db.cursor.execute("select Document_link from Uploads where Course_code = ?", (self.course_code))
+            for row in db.cursor.fetchall():
                 self.up_link.append(row[0])
 
-            query.execute("select Date_of_Upload from Uploads where Course_code=? ",(self.course_code))
-            for row in query.fetchall():
+            db.cursor.execute("select Date_of_Upload from Uploads where Course_code=? ",(self.course_code))
+            for row in db.cursor.fetchall():
                 if row[0] is None:
                     self.up_date.append("No Date")
                 else:
@@ -44,10 +43,10 @@ class upload:
     def insert_upload(self,header,type,description,link,course_code):
             self.today=datetime.now()
             self.today=self.today.strftime("%Y-%m-%d %H:%M:%S")
-            query.execute("select count(*) from Uploads")
-            id=query.fetchone()[0]+1
-            query.execute("""INSERT INTO Uploads(Upload_ID,Upload_header,Upload_type,Upload_description,Document_link,Course_code,Date_of_Upload) VALUES(?,?,?,?,?,?,?)""",(id,header,type,description,link,course_code,self.today))
-            connection.commit()
+            db.cursor.execute("select count(*) from Uploads")
+            self.id=db.cursor.fetchone()[0]+1
+            db.cursor.execute("""INSERT INTO Uploads(Upload_ID,Upload_header,Upload_type,Upload_description,Document_link,Course_code,Date_of_Upload) VALUES(?,?,?,?,?,?,?)""",(self.id,header,type,description,link,course_code,self.today))
+            db.cursor.commit()
 
 
         
