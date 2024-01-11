6e3fa88b6d92d850a0c31f8aa3e229cdd95dce3d
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2024-01-11 05:27:35 +0200

    Update Teacher.py

--- classes/Teacher.py
+++ classes/Teacher.py
@@ -1,19 +1,18 @@
 from users import users
-import pyodbc
-connection=pyodbc.connect('Driver={SQL Server};SERVER=KAREEM;DATABASE=Coursiz;Trusted_Connection=yes')
-query = connection.cursor()
+from databaseConnection import db
+db=db()
 class teacher(users):
 
     def __init__(self, teacher_id):
         self.id = teacher_id
     def sign_up(self):
-        query.execute('INSERT INTO Teachers (teacher_id, course_code) VALUES (?, ?)', ( self.teacher_id, self.course_code))
-        connection.commit()
+        db.cursor.execute('INSERT INTO Teachers (teacher_id, course_code) VALUES (?, ?)', ( self.teacher_id, self.course_code))
+        db.connection.commit()
     
     def get_courses(self):
-        query.execute("select Course_Code from Courses where TeacherID = ?", (self.id))
+        db.cursor.execute("select Course_Code from Courses where TeacherID = ?", (self.id))
         fetched=[]
-        for i in query.fetchall():
+        for i in db.cursor.fetchall():
             i=i[0]
             fetched.append(i)
         return fetched
