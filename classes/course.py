b6fa31f6d57e8f52b4bb9d7d46475c8dc2f664ad
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2024-01-11 05:26:30 +0200

    Update course.py

--- classes/course.py
+++ classes/course.py
@@ -1,7 +1,6 @@
 import pyodbc
-connection=pyodbc.connect('Driver={SQL Server};SERVER=KAREEM;DATABASE=Coursiz;Trusted_Connection=yes')
-query = connection.cursor()
-
+from databaseConnection import db
+db=db()
 class Course_class:
     def __init__(self, courseName, yearTeached, Semester, Capacity,teacher_name):
         self.courseName = courseName
@@ -16,22 +15,22 @@ class Course_class:
             return False
         else:
             first_name,second_name=self.teacher_name.split()[0],self.teacher_name.split()[1]
-            query.execute("SELECT Email FROM Users WHERE First_Name = ? and Last_Name = ?", (first_name, second_name))
-            teacher_email = query.fetchone()[0]
+            db.cursor.execute("SELECT Email FROM Users WHERE First_Name = ? and Last_Name = ?", (first_name, second_name))
+            teacher_email = db.cursor.fetchone()[0]
             if teacher_email == None:
                 return False
             else:
-                query.execute("SELECT TeacherID FROM Teachers WHERE Email = ?", (teacher_email))
-                teacher_id = query.fetchone()[0]
-                query.execute("SELECT COUNT(*) FROM Courses")
-                query.execute("SELECT COUNT(*) FROM Courses")
-                number_of_rows = query.fetchone()[0]
+                db.cursor.execute("SELECT TeacherID FROM Teachers WHERE Email = ?", (teacher_email))
+                teacher_id = db.cursor.fetchone()[0]
+                db.cursor.execute("SELECT COUNT(*) FROM Courses")
+                db.cursor.execute("SELECT COUNT(*) FROM Courses")
+                number_of_rows = db.cursor.fetchone()[0]
                 Course_code='Cs'+str(number_of_rows+1)
-                query.execute("insert into Courses(Name, Year, Semester, Course_Code, Capacity, Number_enrolled,TeacherID) values (?, ?, ?, ?, ?, ?,?)", (self.courseName, self.yearTeached, self.Semester, Course_code, self.Capacity,self.Number_enrolled,teacher_id))
-                query.commit()
+                db.cursor.execute("insert into Courses(Name, Year, Semester, Course_Code, Capacity, Number_enrolled,TeacherID) values (?, ?, ?, ?, ?, ?,?)", (self.courseName, self.yearTeached, self.Semester, Course_code, self.Capacity,self.Number_enrolled,teacher_id))
+                db.cursor.commit()
                 return True
 
     def deleteCourse(self,Course_Code):
-        query.execute('''delete from Courses where Course_Code = ?''', (Course_Code))
-        query.commit()
+        db.cursor.execute('''delete from Courses where Course_Code = ?''', (Course_Code))
+        db.cursor.commit()
 
