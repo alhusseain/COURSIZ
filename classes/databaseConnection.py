1c27f0726f8de24712aefbcc6d3049c7ff916608
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2024-01-11 05:26:03 +0200

    Update databaseConnection.py

--- classes/databaseConnection.py
+++ classes/databaseConnection.py
@@ -1,16 +1,12 @@
 import pyodbc
-
 # first install pyodbc using pip install pyodbc
 # get the server name SQL Server
 # create a new database in SQL Server
 # then execute sql commands in the "SQL code.sql" file
 # replace the server with your server name
 # replace the database with your database name
-connectionString = (
-    r'DRIVER={SQL Server};'
-    r'SERVER= server;'
-    r'DATABASE= databaseName;'
-    r'Trusted_Connection=yes;'
-)
-connection = pyodbc.connect(connectionString)
-cursor = connection.cursor()
+class db:
+    def __init__(self):
+        self.connectionString = ('Driver={SQL Server};SERVER=DESKTOP-9IHIA03;DATABASE=Coursiz;Trusted_Connection=yes')
+        self.connection = pyodbc.connect(self.connectionString)
+        self.cursor = self.connection.cursor()
