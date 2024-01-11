60240fae6f575637658e1872a8b4e9c49023ce58
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2024-01-11 04:54:43 +0200

    Update Students.py

--- classes/Students.py
+++ classes/Students.py
@@ -1,18 +1,17 @@
 from users import users
-import pyodbc
-connection=pyodbc.connect('Driver={SQL Server};SERVER=KAREEM;DATABASE=Coursiz;Trusted_Connection=yes')
-query = connection.cursor()
+from databaseConnection import db
+db=db()
 class students(users):
     def __init__(self,id):
         self.id=id
     def addStudentToCourse(self,Course_Code):
-        query.execute("SELECT Course_Code FROM Courses WHERE Course_Code = ?", (Course_Code))
-        fetched=query.fetchone()
+        db.cursor.execute("SELECT Course_Code FROM Courses WHERE Course_Code = ?", (Course_Code))
+        fetched=db.cursor.fetchone()
         if fetched is None:
             return False
         else:
-            query.execute("select Capacity,Number_enrolled from Courses where Course_Code = ?", (Course_Code))
-            fetched=query.fetchone()
+            db.cursor.execute("select Capacity,Number_enrolled from Courses where Course_Code = ?", (Course_Code))
+            fetched=db.cursor.fetchone()
             if fetched[0] is None:
                 self.Number_enrolled=0
             else: self.Number_enrolled=fetched[0]
@@ -20,22 +19,22 @@ class students(users):
             # if self.Number_enrolled >= self.Capacity:
             #     return False
             # else:
-            query.execute("update Courses set Number_enrolled = ? where Course_Code = ?", (self.Number_enrolled + 1 , Course_Code))
-            query.commit()
-            query.execute("insert into Enroll_in(Student_ID,Course_Code) values(?,?)", (self.id, Course_Code))
-            query.commit()
+            db.cursor.execute("update Courses set Number_enrolled = ? where Course_Code = ?", (self.Number_enrolled + 1 , Course_Code))
+            db.cursor.commit()
+            db.cursor.execute("insert into Enroll_in(Student_ID,Course_Code) values(?,?)", (self.id, Course_Code))
+            db.cursor.commit()
             return True
     
     def removeStudentFromCourse(self,Course_Code):
-        query.execute('''update Courses set Number_enrolled = ? where Course_Code = ?''', (self.Number_enrolled - 1 , Course_Code))
-        query.commit()
-        query.execute('''delete from Enroll_in where Student_ID = ? and Course_Code = ?''', (self.id, Course_Code))
-        query.commit()
+        db.cursor.execute('''update Courses set Number_enrolled = ? where Course_Code = ?''', (self.Number_enrolled - 1 , Course_Code))
+        db.cursor.commit()
+        db.cursor.execute('''delete from Enroll_in where Student_ID = ? and Course_Code = ?''', (self.id, Course_Code))
+        db.cursor.commit()
     
     def get_courses(self):
-        query.execute("select Course_Code from Enroll_in where Student_ID = ?", (self.id))
+        db.cursor.execute("select Course_Code from Enroll_in where Student_ID = ?", (self.id))
         fetched=[]
-        for i in query.fetchall():
+        for i in db.cursor.fetchall():
             i=i[0]
             fetched.append(i)
         return fetched
